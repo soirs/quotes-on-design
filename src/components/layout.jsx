dd65fa6f644bd2720a9341ecb0bede4e16cc3217
Frank Richard Semakula <franksemakula@gmail.com>
2020-02-11 17:41:02 +0100

    :bug: still persists

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,14 +1,22 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import PropTypes from "prop-types";
 import React from "react";
+import Div100vh from "react-div-100vh";
 import Helmet from "react-helmet";
 import HelmetProps from "./helmet";
 
 const Layout = ({ children }) => {
+  const isSSR = typeof window === "undefined";
   return (
     <>
-      <Helmet {...HelmetProps} />
-      <main className="layout">{children}</main>
+      {!isSSR && (
+        <React.Suspense fallback={<div />}>
+          <Div100vh>
+            <Helmet {...HelmetProps} />
+            <main className="layout">{children}</main>
+          </Div100vh>
+        </React.Suspense>
+      )}
     </>
   );
 };
