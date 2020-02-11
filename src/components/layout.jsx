3d02a2c3289944f0cb3fd189f2ab7a66688b8951
Frank Richard Semakula <franksemakula@gmail.com>
2020-02-11 17:02:03 +0100

    :bug:

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -5,23 +5,27 @@ import Helmet from "react-helmet";
 import HelmetProps from "./helmet";
 
 const Layout = ({ children }) => {
-  // We listen to the resize event
-  window.addEventListener("resize", () => {
-    // We execute the same script as before
-    const vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty("--vh", `${vh}px`);
-  });
   const isSSR = typeof window === "undefined";
   return (
     <>
       {!isSSR && (
         <React.Suspense fallback={<div />}>
           <Helmet {...HelmetProps} />
+          <HeightFix />
           <main className="layout">{children}</main>
         </React.Suspense>
       )}
     </>
   );
+};
+
+const HeightFix = () => {
+  // We listen to the resize event
+  return window.addEventListener("resize", () => {
+    // We execute the same script as before
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+  });
 };
 
 Layout.propTypes = {
