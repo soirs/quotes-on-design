60e3ebf0ac756c5725de7eb340504d8f66901661
Frank Richard Semakula <franksemakula@gmail.com>
2020-02-11 16:52:15 +0100

    :bug: retry layout on mobile

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -5,10 +5,21 @@ import Helmet from "react-helmet";
 import HelmetProps from "./helmet";
 
 const Layout = ({ children }) => {
+  // We listen to the resize event
+  window.addEventListener("resize", () => {
+    // We execute the same script as before
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+  });
+  const isSSR = typeof window === "undefined";
   return (
     <>
-      <Helmet {...HelmetProps} />
-      <main className="layout">{children}</main>
+      {!isSSR && (
+        <React.Suspense fallback={<div />}>
+          <Helmet {...HelmetProps} />
+          <main className="layout">{children}</main>
+        </React.Suspense>
+      )}
     </>
   );
 };
