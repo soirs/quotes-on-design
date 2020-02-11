8df3fea3287717f8d2f869abd4ff26f3e978fcec
Frank Richard Semakula <franksemakula@gmail.com>
2020-02-11 18:04:52 +0100

    :bug: mobile view broken continued

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,7 +1,6 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import PropTypes from "prop-types";
 import React from "react";
-import Div100vh from "react-div-100vh";
 import Helmet from "react-helmet";
 import HelmetProps from "./helmet";
 
@@ -11,10 +10,10 @@ const Layout = ({ children }) => {
     <>
       {!isSSR && (
         <React.Suspense fallback={<div />}>
-          <Div100vh>
-            <Helmet {...HelmetProps} />
-            <main className="layout">{children}</main>
-          </Div100vh>
+          {/* <Div100vh> */}
+          <Helmet {...HelmetProps} />
+          <main className="layout">{children}</main>
+          {/* </Div100vh> */}
         </React.Suspense>
       )}
     </>
