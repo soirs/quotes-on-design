0ba081f2bdcbab78f318be2e70c1a858ce9007ae
Frank Richard Semakula <franksemakula@gmail.com>
2020-02-11 15:06:53 +0100

    :bug: continued issue

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -5,12 +5,6 @@ import Helmet from "react-helmet";
 import HelmetProps from "./helmet";
 
 const Layout = ({ children }) => {
-  // We listen to the resize event
-  window.addEventListener("resize", () => {
-    // We execute the same script as before
-    const vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty("--vh", `${vh}px`);
-  });
   return (
     <>
       <Helmet {...HelmetProps} />
